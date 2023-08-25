764f4504cddfb72388dc23c57b8696b06ec23587
Prerana-V <vprerana55@gmail.com>
2023-08-25 22:58:52 +0530

    updated folder structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import NavbarComponent from "@/components/navbar/NavbarComponent";
+import NavbarComponent from "@/components/NavbarComponent";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
